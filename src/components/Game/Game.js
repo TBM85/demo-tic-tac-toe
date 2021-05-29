4e7157111e3866b90532d9fc456edec075a890a9
TBM85 <tballesterm@gmail.com>
2021-05-29 16:46:46 -0400

    Create a square structure

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Square from '../Square/Square';
 
 const Game = () => {
   return (
     <div className="game">
-      <div className="game-board"></div>
+      <div className="game-board"><Square /></div>
       <div className="game-info"></div>
     </div>
   );
